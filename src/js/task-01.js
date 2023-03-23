d3b9b81c1dab8c61fe85c8c1f2437a41a0a8c478
michalmazurdev <m.mazur40@gmail.com>
2023-03-23 01:20:49 +0100

    first task done

---
+++ src/js/task-01.js
@@ -0,0 +1,11 @@
+const categories = document.querySelectorAll(".item");
+
+console.log(`Number of categories: ${categories.length}`);
+
+for (let i = 0; i < categories.length; i++) {
+  console.log(
+    `Category: ${categories[i].firstElementChild.innerText}
+    Elements: ${categories[i].lastElementChild.children.length}`
+  );
+}
+console.log(categories[0].lastElementChild);
