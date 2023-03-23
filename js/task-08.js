f6507b5807f12590e6a2f30b75e9c7bdc7cc0358
michalmazurdev <m.mazur40@gmail.com>
2023-03-24 00:26:18 +0100

    8th taks done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("You cannot leave any of the fields empty.");
+  }
+  const credentialsProvided = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(credentialsProvided);
+  formEl.reset();
+}
+formEl.addEventListener("submit", handleSubmit);
